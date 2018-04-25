73bd9d1abe366dba2ce64d4bc7ff219d13de874b
NAVDEEP SIDHU <sidhu44@gmail.com>
2018-04-25 20:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
-function takeANumber(katzDeli, name ){
-  katzDeli.push(name);
-  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber(katzDeliLine, name ){
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
